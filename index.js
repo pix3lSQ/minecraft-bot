2d1df18d9865be9c6715d08f7a7953efbbfb3b39
pix3lSQ <168378324+pix3lSQ@users.noreply.github.com>
2026-03-05 03:29:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function startBot() {
   console.log("Starting bot...")
 
   const bot = mineflayer.createBot({
-    host: 'YOURSERVER.aternos.host', // change this
-    port: 12345,                     // change this
+    host: 'grison.aternos.host', // your host
+    port: 13422,                 // your port
     username: 'AFK_Bot',
-    version: false                   // auto detect version
+    version: false
   })
 
   bot.on('login', () => {
@@ -16,42 +16,23 @@ function startBot() {
   })
 
   bot.on('spawn', () => {
-    console.log("Bot spawned in the server!")
+    console.log("Bot spawned!")
 
-    bot.chat("AFK Bot is now online!")
+    bot.chat("AFK Bot online!")
 
-    // Random movement so server doesn't kick it
     setInterval(() => {
-      const actions = ['forward', 'back', 'left', 'right']
-
-      const action = actions[Math.floor(Math.random() * actions.length)]
-
-      bot.setControlState(action, true)
-
-      setTimeout(() => {
-        bot.setControlState(action, false)
-      }, 2000)
-
       bot.setControlState('jump', true)
-
-      setTimeout(() => {
-        bot.setControlState('jump', false)
-      }, 500)
-
+      setTimeout(() => bot.setControlState('jump', false), 500)
     }, 10000)
-  })
-
-  bot.on('kicked', (reason) => {
-    console.log("Bot was kicked:", reason)
-  })
-
-  bot.on('error', (err) => {
-    console.log("Error:", err)
   })
 
   bot.on('end', () => {
     console.log("Bot disconnected. Reconnecting in 15 seconds...")
     setTimeout(startBot, 15000)
+  })
+
+  bot.on('error', (err) => {
+    console.log("Error:", err)
   })
 }
 
